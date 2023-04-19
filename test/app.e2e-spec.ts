7bc7cd1ecf8ae9076f1993700fa43896a7df7153
seanlin1125 <sean821125@gmail.com>
2023-04-19 15:32:16 +0800

    feat: add sign up in test and token setup

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,9 @@
-import { Test } from '@nestjs/testing'
-import { AppModule } from '../src/app.module'
 import { INestApplication, ValidationPipe } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import * as pactum from 'pactum'
+import { AppModule } from '../src/app.module'
 import { PrismaService } from '../src/prisma/prisma.service'
+import { AuthDto } from '../src/auth/dto'
 
 describe('App e2e', () => {
   let app: INestApplication
@@ -14,13 +16,108 @@ describe('App e2e', () => {
     app = moduleRef.createNestApplication()
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
     await app.init()
+    await app.listen(3333)
 
     prisma = app.get(PrismaService)
     await prisma.cleanDb()
+    pactum.request.setBaseUrl('http://localhost:3333')
   })
 
   afterAll(() => {
     app.close()
   })
 
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'sean@gmail.com',
+      password: '123',
+    }
+    describe('Signup', () => {
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400)
+      })
+      it('should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400)
+      })
+      it('should throw if no body provided', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400)
+      })
+      it('should signup', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201)
+      })
+    })
+
+    describe('Signin', () => {
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400)
+      })
+      it('should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400)
+      })
+      it('should throw if no body provided', () => {
+        return pactum.spec().post('/auth/signin').expectStatus(400)
+      })
+      it('should signin', () => {
+        return (
+          pactum
+            .spec()
+            .post('/auth/signin')
+            .withBody(dto)
+            .expectStatus(200)
+            // .inspect() // 可以用來看回傳的資料
+            .stores('userAt', 'access_token') // 自定義userAt這個變數去存body裡面的access_token的值
+        )
+      })
+    })
+  })
+
+  describe('User', () => {
+    describe('Get me', () => {
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({ Authorization: 'Bearer $S{userAt}' }) // $S代表上面儲存的"stores"
+          .expectStatus(200)
+      })
+    })
+
+    describe('Edit user', () => {})
+  })
+
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {})
+    describe('Get bookmarks', () => {})
+    describe('Get bookmark by id', () => {})
+    describe('Edit bookmark', () => {})
+    describe('Delete bookmark', () => {})
+  })
 })
