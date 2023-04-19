99a18bca7e92baad2fad85011a2d19b3f8f7121a
seanlin1125 <sean821125@gmail.com>
2023-04-19 13:26:11 +0800

    feat: add get user route

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
 import { BookmarkModule } from './bookmark/bookmark.module'
 import { PrismaModule } from './prisma/prisma.module'
+import { UserController } from './user/user.controller'
 
 @Module({
   imports: [
@@ -14,5 +15,6 @@ import { PrismaModule } from './prisma/prisma.module'
     BookmarkModule,
     PrismaModule,
   ],
+  controllers: [UserController],
 })
 export class AppModule {}
