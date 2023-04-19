852971ab60e05a1d8142ea3cefa56cce6b9593b9
seanlin1125 <sean821125@gmail.com>
2023-04-19 13:48:34 +0800

    feat: add decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthDto } from './dto'
 
@@ -11,11 +11,14 @@ export class AuthController {
   // constructor(authService: AuthService) {
   //   this.authService = authService
   // }
+
+  // post request 預設回傳201
   @Post('signup') // /auth/signup
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto)
   }
 
+  @HttpCode(HttpStatus.OK) // 這樣就可以把 post預設的201改成200，因為這邊只是登入沒有創建資料
   @Post('signin') // /auth/signin
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto)
